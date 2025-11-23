7278dfa4dc5945680483ee8d7a36a1f3f6a29364
Mackeyth28 <thapkrncingam@gmail.com>
2025-11-23 20:53:34 +0700

    Lab

---
+++ Lab2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Lab2 {
+    public static void main(String[] args) {
+        // Create a Scanner object to read user input
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("--- Rectangle Area Calculator Program ---");
+
+        // 1. Get the Width as a double
+        System.out.print("Enter the Width: ");
+        // Use nextDouble() to read the decimal value
+        double width = scanner.nextDouble();
+
+        // 2. Get the Height as a double
+        System.out.print("Enter the Height: ");
+        double height = scanner.nextDouble();
+
+        // 3. Calculate the Area
+        double area = width * height;
+
+        // 4. Display the results
+        System.out.println("----------------------------------------");
+        System.out.println("Input Width: " + width);
+        System.out.println("Input Height: " + height);
+        // Display the calculated area
+        System.out.println("The Area of the rectangle is: " + area);
+        System.out.println("----------------------------------------");
+
+        // Close the Scanner
+        scanner.close();
+ }
+}
+
